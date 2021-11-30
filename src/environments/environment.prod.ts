c0cb362e1809b100ca4d84ee53cbfebb59977200
jbernal <j.bernal@upm.es>
2021-11-30 16:12:17 +0100

    added environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
+import {name, version} from '../../package.json';
+
 export const environment = {
-  production: true
+  production: true,
+  NAME: name,
+  VERSION: version,
+  REST_USER: 'https://betca-tpv-user.herokuapp.com',
+  REST_CORE: 'https://betca-tpv-core.herokuapp.com',
+  REST_CUSTOMER_SUPPORT: 'https://betca-tpv-customer-support.herokuapp.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,13 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import {name, version} from '../../package.json';
 export const environment = {
-  production: false
+  production: false,
+  NAME: name,
+  VERSION: version,
+  REST_USER: 'http://localhost:8081',
+  REST_CORE: 'http://localhost:8082'
 };
 
 /*
