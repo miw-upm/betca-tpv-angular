4e288404e931e3e9e0f3852e8f7e82b5b7f2f9dc
DavidYareth <davidyareth@gmail.com>
2024-02-21 21:17:47 +0100

    Add adviser menu handler structure miw-upm/betca-tpv#358

--- src/app/home/adviser/adviser.component.ts
+++ src/app/home/adviser/adviser.component.ts
@@ -1,8 +1,40 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TagService} from "../../shop/tags/tag.service";
+import { Article} from "../shared/article.model";
 
 @Component({
-  templateUrl: 'adviser.component.html',
-  styleUrls: ['adviser.component.css'],
+  selector: 'app-adviser',
+  templateUrl: './adviser.component.html',
+  styleUrls: ['./adviser.component.css']
 })
-export class AdviserComponent {
+export class AdviserComponent implements OnInit {
+  articles: Article[] = [];
+  selectedTag: string = '';
+
+  constructor(private tagService: TagService, private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.selectedTag = params['tag'];
+      if (this.selectedTag) {
+        this.loadArticles(this.selectedTag);
+      }
+    });
+  }
+
+  loadArticles(tagName: string): void {
+    this.tagService.read(tagName).subscribe(tag => {
+      if (tag) {
+        this.articles = tag.articles;
+      } else {
+        this.articles = [];
+        console.error('Tag not found:', tagName);
+      }
+    });
+  }
+
+  addToBasket(article: Article): void {
+    // TODO: implement
+  }
 }
