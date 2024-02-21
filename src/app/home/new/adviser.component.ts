5459c712f7a76f82803059632d6cd203243e0050
Rodrigo de la Calle <rodricalle@usal.es>
2024-02-21 21:17:01 +0100

    Implement Carrusel in Adviser #374

--- src/app/home/new/adviser.component.ts
+++ src/app/home/new/adviser.component.ts
@@ -1,8 +1,10 @@
 import {Component} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   templateUrl: 'adviser.component.html',
   styleUrls: ['adviser.component.css'],
 })
 export class AdviserComponent {
+
 }
