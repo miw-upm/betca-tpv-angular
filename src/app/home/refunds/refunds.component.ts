14d7a67707bb8393dedec25b6dd699ad4b9629de
Igor Lopez <igor.lopezs@alumni.mondragon.edu>
2024-02-22 18:43:22 +0100

    Create Refund View for client miw-upm/betca-tpv#381

---
+++ src/app/home/refunds/refunds.component.ts
@@ -0,0 +1,41 @@
+import {Component, OnInit} from '@angular/core';
+import {OnlineOrder} from "../shared/online-order.model";
+import {OnlineOrderState} from "../shared/online-order-state";
+
+@Component({
+  selector: 'app-refunds',
+  templateUrl: './refunds.component.html',
+  styleUrls: ['./refunds.component.css']
+})
+export class RefundsComponent implements OnInit {
+
+  displayedColumns = ['id', 'reference', 'state', 'deliveryDate', 'ticketReference', 'actions'];
+  title = 'Refunds of online orders';
+  onlineOrders : OnlineOrder[] = [];
+  onlineOrderStateKeys = Object.values(OnlineOrderState);
+
+  private mockOnlineOrders: OnlineOrder[] = [
+    { reference: "112233ascd", state: OnlineOrderState.PREPARING, deliveryDate: null, ticketReference: "445566grtq"},
+    { reference: "cgfdf45fsf", state: OnlineOrderState.SENT, deliveryDate: null, ticketReference: "w3434dgbbg"},
+    { reference: "2effbfhdgd", state: OnlineOrderState.DELIVERED, deliveryDate: new Date(), ticketReference: "eere4tfgb5"},
+    { reference: "435bknsdff", state: OnlineOrderState.REFUND_REQUESTED, deliveryDate: new Date(), ticketReference: "w3434tfgb5"},
+    { reference: "458dsfdsfs", state: OnlineOrderState.REFUNDED, deliveryDate: new Date(), ticketReference: "eere4w3434"}
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.onlineOrders = [...this.mockOnlineOrders].filter(row => this.showOnlineOrder(row.state));
+  }
+
+  showOnlineOrder(state: OnlineOrderState): boolean {
+    return state == OnlineOrderState.DELIVERED ||
+            state == OnlineOrderState.REFUND_REQUESTED ||
+            state == OnlineOrderState.REFUNDED;
+  }
+
+  refundOnlineOrder(order: OnlineOrder){
+
+  }
+
+}
