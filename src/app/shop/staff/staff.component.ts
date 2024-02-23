8953526ebc7803cde37b6011a8de6ed3f09c2d52
ruliworst <raulfa1503@gmail.com>
2024-02-23 23:02:57 +0100

    models and hours per day UI miw-upm/betca-tpv#391

--- src/app/shop/staff/staff.component.ts
+++ src/app/shop/staff/staff.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import {StaffService} from "./staff.service";
 
-
 @Component({
   templateUrl: 'staff.component.html',
 })
 export class StaffComponent {
   title = 'Staff Records Management';
+  staffHoursPerDay = [];
+  staffHoursPerMonth = [];
 
   constructor(private staffService: StaffService) {
+    this.staffService.mockHoursPerDate().subscribe(data => {
+      this.staffHoursPerDay = data;
+      this.staffHoursPerMonth = data;
+    });
+  }
+
+  formatDate(date : Date) {
+    return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
   }
 }
