17261d27bfa2b8184e9ea251a99e5fdb3ab7b7f0
jdggLgns <josedaniel.gonzalez.garcia@alumnos.upm.es>
2024-02-24 08:32:23 +0100

    miw-upm/betca-tpv#398 Mocked complaints and read service

--- src/app/home/complaints/complaint.service.ts
+++ src/app/home/complaints/complaint.service.ts
@@ -14,6 +14,11 @@ export class ComplaintService {
   constructor(private httpService: HttpService) {
   }
 
+  private mockComplaints: Complaint[] = [
+    { id: '1', barcode: '123456789', description: 'Mocked Complaint 1', registrationDate: new Date() },
+    { id: '2', barcode: '987654321', description: 'Mocked Complaint 2', registrationDate: new Date() },
+  ];
+
   /*create(complaint: Complaint): Observable<Complaint> {
     return this.httpService
       .post(EndPoints.COMPLAINTS, complaint);
@@ -28,20 +33,25 @@ export class ComplaintService {
     };
     return of(mockResponse);
   }
-
   searchAll(): Observable<Complaint[]> {
-    return this.httpService
+       return this.httpService
       .get(EndPoints.COMPLAINTS + ComplaintService.SEARCH);
   }
 
+  /*
   read(id: string): Observable<Complaint> {
     return this.httpService
       .get(EndPoints.COMPLAINTS + '/' + id);
+  }
+  */
+
+  read(id: string): Observable<Complaint> {
+    const complaint = this.mockComplaints.find(c => c.id === id);
+    return of(complaint || {} as Complaint);
   }
 
   delete(id: string): Observable<void> {
     return this.httpService
       .delete(EndPoints.COMPLAINTS + '/' + id);
   }
-
 }
