2e5ac8c0eff9f15457cf8fa40b2662760065f9c4
Daniel Hernández Fajardo <daniel.hernandez@codebay-innovation.com>
2024-02-28 20:09:50 +0100

    Created offers interface mockup miw-upm/betca-tpv#392

---
+++ src/app/shop/offers/offers-search.model.ts
@@ -0,0 +1,7 @@
+import {Article} from "../shared/services/models/article.model";
+
+export class OfferSearch {
+  reference?:String;
+  creationDate?:Date;
+  expiryDate?:Date;
+}
--- src/app/shop/offers/offers.component.ts
+++ src/app/shop/offers/offers.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import {of} from "rxjs";
+import {MatDialog} from "@angular/material/dialog";
+import {OfferService} from "./offers.service";
+import {Offer} from "../shared/services/models/offer.model";
+import {OrderSearch} from "../orders/ordersearch.model";
+import {OfferSearch} from "./offers-search.model";
 
 @Component({
   selector: 'app-offers',
   templateUrl: './offers.component.html',
   styleUrls: ['./offers.component.css']
 })
-export class OffersComponent implements OnInit {
+export class OffersComponent {
+  title = 'Offers management';
+  offers = of([]);
+  public offerSearch: OfferSearch;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private dialog: MatDialog, private offerService: OfferService) {
+    this.resetSearch();
   }
 
+  search(): void {
+    this.offers = this.offerService.search();
+  }
+
+  public resetSearch(): void {
+    this.offerSearch = {};
+    this.search();
+  }
+
+  create(): void {
+    //TODO
+  }
+
+  read(offer: Offer): void {
+    //TODO
+  }
+
+  delete(offer: Offer): void {
+    //TODO
+  }
+
+  update(offer: Offer): void {
+    //TODO
+  }
 }
