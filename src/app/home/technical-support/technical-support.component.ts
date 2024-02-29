7d6aa020e56f643232388d3e21f01f96b4210e93
Hugo Vázquez Docampo <81679649+hugovazquezd@users.noreply.github.com>
2024-02-29 22:43:15 +0100

    import que sobra: miw-upm/betca-tpv#402

--- src/app/home/technical-support/technical-support.component.ts
+++ src/app/home/technical-support/technical-support.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
 import {MatPaginator} from "@angular/material/paginator";
 import {MatTableDataSource} from "@angular/material/table";
 import {MatSort} from '@angular/material/sort';
