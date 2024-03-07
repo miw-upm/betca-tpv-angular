041096af652849744c375574a4151ee971831e0e
Guillermo Navío <100093310+gnavio@users.noreply.github.com>
2024-03-07 20:38:04 +0100

    Update voucher services miw-upm/betca-tpv#405

--- src/app/shop/shared/services/shared-voucher.service.ts
+++ src/app/shop/shared/services/shared-voucher.service.ts
@@ -1,18 +1,22 @@
 import {Injectable} from '@angular/core';
+import {VouchersService} from 'app/shop/vouchers/vouchers.service';
 import {EMPTY, Observable} from 'rxjs';
+import {Voucher} from './models/voucher.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SharedVoucherService {
 
+  constructor(private voucherService: VouchersService) {
+  }
+
   printVoucher(value: number): Observable<any> {
     console.log('Printing voucher with value: ' + value);
     return EMPTY; // TODO create and print voucher
   }
 
-  consumeVoucher(reference: string): Observable<any> {
-    console.log('Consuming voucher with reference: ' + reference);
-    return EMPTY; // TODO consume voucher
+  consumeVoucher(reference: string): Observable<Voucher> {
+    return this.voucherService.update(reference);
   }
 }
