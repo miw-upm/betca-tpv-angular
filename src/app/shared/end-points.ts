b0bf3a9add019b5cf4042db6593cfdb4aaa236f4
JorgeDSDP <jorge.dasilva@alumni.mondragon.edu>
2024-03-12 23:19:20 +0100

    Connected admin and user online order screens to CORE miw-upm/betca-tpv#457

--- src/app/shared/end-points.ts
+++ src/app/shared/end-points.ts
@@ -19,5 +19,5 @@ export class EndPoints {
   static VAT = environment.REST_CORE + '/vat';
   static ORDERS: string = environment.REST_CORE + '/orders';
   static GIFT_TICKETS = environment.REST_CORE + '/gift-tickets';
-
+  static ONLINE_ORDERS = environment.REST_CORE + '/online-orders';
 }
