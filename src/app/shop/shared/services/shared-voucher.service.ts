341247e1e9299320148a981ce7cea67b4489f4bd
Guillermo Navío <100093310+gnavio@users.noreply.github.com>
2024-03-27 13:50:28 +0100

    Add printVoucher shared service miw-upm/betca-tpv#406

--- src/app/shop/shared/services/shared-voucher.service.ts
+++ src/app/shop/shared/services/shared-voucher.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from '@angular/core';
 import {VouchersService} from 'app/shop/vouchers/vouchers.service';
-import {EMPTY, Observable} from 'rxjs';
+import {Observable, switchMap} from 'rxjs';
 import {Voucher} from './models/voucher.model';
+import {VoucherCreation} from 'app/shop/vouchers/voucher-creation.model';
 
 @Injectable({
   providedIn: 'root',
@@ -11,9 +12,11 @@ export class SharedVoucherService {
   constructor(private voucherService: VouchersService) {
   }
 
-  printVoucher(value: number): Observable<any> {
-    console.log('Printing voucher with value: ' + value);
-    return EMPTY; // TODO create and print voucher
+  printVoucher(value: number): Observable<void> {
+    const voucher: VoucherCreation = { value: value };
+    return this.voucherService.create(voucher).pipe(
+      switchMap((voucher: Voucher) => this.voucherService.readReceipt(voucher.reference))
+    );
   }
 
   consumeVoucher(reference: string): Observable<Voucher> {
