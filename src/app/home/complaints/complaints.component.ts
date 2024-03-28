73347e3d0671be5be712bfad1f580abd46312c5e
jdggLgns <josedaniel.gonzalez.garcia@alumnos.upm.es>
2024-03-28 20:25:45 +0100

    Created openReplyDialog and replyAndCloseComplaint methods in complaints component  miw-upm/betca-tpv#477

--- src/app/home/complaints/complaints.component.ts
+++ src/app/home/complaints/complaints.component.ts
@@ -1,21 +1,31 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import {of} from 'rxjs';
+import {of, Observable} from 'rxjs';
 
+import { AuthService } from '@core/auth.service'; 
 import {ComplaintCreationDialogComponent} from './complaint-creation-dialog.component';
 import {ComplaintService} from './complaint.service';
 import {Complaint} from './complaint.model';
 import {ReadDetailDialogComponent} from '@shared/dialogs/read-detail.dialog.component';
+import { ComplaintReplyDialogComponent } from './complaint-reply-dialog.component';
 
 @Component({
-  templateUrl: 'complaints.component.html'
+  selector: 'app-complaints',
+  templateUrl: 'complaints.component.html',
+
 })
 export class ComplaintsComponent {
   title = 'Complaints management';
-  complaints = of([]);
+  complaints$: Observable<Complaint[]>; 
+  isManager: boolean;
 
-  constructor(private dialog: MatDialog, private complaintService: ComplaintService, private snackBar: MatSnackBar) {
+  constructor(private dialog: MatDialog, private complaintService: ComplaintService,  private authService: AuthService, private snackBar: MatSnackBar) {
+  }
+
+  ngOnInit(): void {
+    this.complaints$ = this.complaintService.searchAll(); 
+    this.isManager = this.authService.untilManager(); 
   }
 
   create(): void {
@@ -26,7 +36,7 @@ export class ComplaintsComponent {
   }
 
   searchAll(): void {
-    this.complaints = this.complaintService.searchAll();
+    this.complaints$ = this.complaintService.searchAll();
   }
 
   read(complaint: Complaint): void {
@@ -37,6 +47,7 @@ export class ComplaintsComponent {
       }
     });
   }
+
   delete(complaint: Complaint): void {
     this.complaintService
       .delete(complaint.id)
@@ -69,5 +80,29 @@ export class ComplaintsComponent {
         });
       }
     });
+  }
+
+  openReplyDialog(complaintId: string): void {
+    const dialogRef = this.dialog.open(ComplaintReplyDialogComponent, {
+      width: '500px',
+      data: { complaintId }
+    });
+  
+    dialogRef.afterClosed().subscribe(reply => {
+      if (reply) {
+        this.replyAndCloseComplaint(complaintId, reply);
+      }
+    });
+  }
+
+  replyAndCloseComplaint(complaintId: string, reply: string): void {
+    if (reply.trim()) {
+      this.complaintService.closeComplaint(complaintId, reply).subscribe(() => {
+        this.snackBar.open('Complaint has been successfully replied to and closed.', 'Close', { duration: 3000 });
+        this.searchAll();
+      });
+    } else {
+      this.snackBar.open('Reply cannot be empty.', 'Close', { duration: 3000 });
+    }
   }
 }
