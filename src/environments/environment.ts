824842f83dc66e7e5476a0846650d45bc0cdbb03
JBernal <j.bernal@upm.es>
2025-02-10 18:42:18 +0100

    Version 4.8.0-SNAPSHOT & clean environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,8 +9,7 @@ export const environment = {
   NAME: pkg.name,
   VERSION: pkg.version,
   REST_USER: 'http://localhost:8081',
-  REST_CORE: 'http://localhost:8082',
-  REST_CUSTOMER_SUPPORT: 'http://localhost:8083'
+  REST_CORE: 'http://localhost:8082'
 };
 
 /*
