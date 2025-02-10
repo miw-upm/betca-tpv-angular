f12814abb2f2b4b4a9fa3e98c32658fe1b7ffc75
JBernal <j.bernal@upm.es>
2025-02-10 06:15:54 +0100

    Version 4.6.0-SNAPSHOT  curso 2024-25

--- src/app/app-routes.module.ts
+++ src/app/app-routes.module.ts
@@ -11,5 +11,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {
+export class AppRoutesModule {
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import {BrowserModule} from "@angular/platform-browser";
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {CoreModule} from '@core/core.module';
-import {AppRoutingModule} from './app-routing.module';
+import {AppRoutesModule} from './app-routes.module';
 import {AppComponent} from './app.component';
 
 
 @NgModule({
   imports: [
-    AppRoutingModule,
+    AppRoutesModule,
     BrowserModule,
     BrowserAnimationsModule,
     CoreModule,
