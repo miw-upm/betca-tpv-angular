38dc7c04df5bb43b14026e3bae808441a2effa1e
estedosunidos <ivanestebanvargasmartinez@gmail.com>
2025-02-21 13:13:44 +0100

    adds UI prototype for user story tags

--- src/app/core/end-points.ts
+++ src/app/core/end-points.ts
@@ -3,6 +3,7 @@ import {environment} from '@env';
 export class EndPoints {
     static readonly PROVIDERS = environment.REST_CORE + '/providers';
     static readonly ARTICLES = environment.REST_CORE + '/articles';
+    static readonly TAGS = environment.REST_CORE + '/tags';
     static readonly CASHIERS = environment.REST_CORE + '/cashiers';
     static readonly CASHIERS_LAST = EndPoints.CASHIERS + '/last';
     static readonly TICKETS = environment.REST_CORE + '/tickets';
