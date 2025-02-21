9d369c39f9b4a6a1598d2fc11082131a4e88641c
Miguel Ángel <madelfresno@gmail.com>
2025-02-21 13:35:05 +0100

    Create CRUD dialogs #561

--- src/app/features/shop/budgets/models/budget.ts
+++ src/app/features/shop/budgets/models/budget.ts
@@ -1,7 +1,9 @@
 import { Shopping } from "../../cashier-opened/shopping-cart/shopping.model";
 
-export interface Budget {
+export type Budget = {
   reference: string;
   creationDate: Date;
   shoppings: Shopping[];
-}
+};
+
+export type BudgetSearch = Partial<Pick<Budget, "reference" | "creationDate">>;
