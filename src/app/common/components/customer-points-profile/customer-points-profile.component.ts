b633b03fecfac015937cbdba93b483ce0c25d6fb
joralvmel <joralvmel@gmail.com>
2025-02-21 19:55:05 +0100

    miw-upm/betca-tpv#556 add search component for customer points

--- src/app/common/components/customer-points-profile/customer-points-profile.component.ts
+++ src/app/common/components/customer-points-profile/customer-points-profile.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomerPointsService } from '../../../features/shop/cashier-opened/shopping-cart/customer-points/customer-points.service';
 import { CustomerPoints } from '../../../features/shop/cashier-opened/shopping-cart/customer-points/customer-points.model';
 import { MatDividerModule } from '@angular/material/divider';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'customer-points-profile',
@@ -11,18 +12,25 @@ import { MatDividerModule } from '@angular/material/divider';
     templateUrl: './customer-points-profile.component.html',
     styleUrls: ['./customer-points-profile.component.css']
 })
-export class CustomerPointsProfileComponent implements OnInit {
-    points: number = 33;
-    lastUpdated: Date | null = new Date('2025-01-02T10:00:00Z');
+export class CustomerPointsProfileComponent implements OnInit, OnDestroy {
+    points: number = 0;
+    lastUpdated: Date | null = null;
+    private subscription: Subscription;
 
     constructor(private customerPointsService: CustomerPointsService) {}
 
     ngOnInit(): void {
+        this.subscription = this.customerPointsService.customerPoints$.subscribe((cp: CustomerPoints) => {
+            if (cp) {
+                this.points = cp.value;
+                this.lastUpdated = cp.lastDate;
+            }
+        });
+    }
 
-        const cp: CustomerPoints = this.customerPointsService.getCurrentPoints();
-        if (cp) {
-            this.points = cp.value;
-            this.lastUpdated = cp.lastDate;
+    ngOnDestroy(): void {
+        if (this.subscription) {
+            this.subscription.unsubscribe();
         }
     }
 }
