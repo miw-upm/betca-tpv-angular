c7d45b5f131c3bd6067815be2cc4593615b571d0
LuisLoboR16 <Lwolf95@hotmail.com>
2025-02-21 07:46:47 -0500

    Added create and update to UI miw-upm/betca-tpv#560

---
+++ src/app/features/shop/cashier-opened/tickets/models/tickets.model.ts
@@ -0,0 +1,3 @@
+export interface Tickets {
+    reference: string;
+}
--- src/app/features/shop/invoices/components/invoice-updating/invoice-updating.component.ts
+++ src/app/features/shop/invoices/components/invoice-updating/invoice-updating.component.ts
@@ -1,11 +1,50 @@
-import { Component } from "@angular/core";
+import {Component, Inject} from "@angular/core";
+import {Invoice} from "../../models/invoice.model";
+import {
+    MAT_DIALOG_DATA,
+    MatDialog,
+    MatDialogActions,
+    MatDialogClose,
+    MatDialogContent,
+    MatDialogTitle
+} from "@angular/material/dialog";
+import {InvoiceService} from "../../services/invoice.service";
+import {AuthService} from "@core/services/auth.service";
+import {MatButton} from "@angular/material/button";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {FormsModule} from "@angular/forms";
 
 @Component({
     templateUrl: "./invoice-updating.component.html",
     standalone: true,
+    imports: [
+        MatButton,
+        MatDialogActions,
+        MatDialogClose,
+        MatDialogContent,
+        MatDialogTitle,
+        MatFormField,
+        MatInput,
+        MatLabel,
+        FormsModule
+    ],
     styleUrls: ["./invoice-updating.component.css"]
 })
 
-export class InvoiceUpdatingComponent{
+export class InvoiceUpdatingComponent {
+    title: string;
+    invoice: Invoice;
 
+    constructor(@Inject(MAT_DIALOG_DATA) data: Invoice, private invoiceService: InvoiceService, private dialog: MatDialog, private auth: AuthService) {
+        this.title = 'Update Invoice';
+        this.invoice = data ? data : {
+            identity: undefined,
+            creationDate: new Date(),
+            baseTax: undefined,
+            taxValue: undefined,
+            user: this.auth.getUser(),
+            ticket: undefined
+        };
+    }
 }
