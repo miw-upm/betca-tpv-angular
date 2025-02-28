8665d275ae9828ba7812624f9314b582582b7846
yinanshangguan <yinanshangguan0914@gmail.com>
2025-02-28 22:13:55 +0100

    Modify the code interface of reviews miw-upm/betca-tpv#570

--- src/app/features/home/reviews/reviews.component.ts
+++ src/app/features/home/reviews/reviews.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {MatIcon} from "@angular/material/icon";
-import {NgClass} from "@angular/common";
+import {NgClass, NgForOf} from "@angular/common";
 import {FormsModule} from "@angular/forms";
 
 @Component({
@@ -9,16 +9,43 @@ import {FormsModule} from "@angular/forms";
   imports: [
     MatIcon,
     NgClass,
-    FormsModule
+    FormsModule,
+    NgForOf
   ],
   styleUrls: ['./reviews.component.css']
 })
 export class ReviewsComponent {
   reviews = [
-    { article: { name: 'Product A' }, stars: 3, opinion: 'Good product!' },
-    { article: { name: 'Product B' }, stars: 5, opinion: 'Excellent!' },
+    {
+      article: {
+        name: 'Pedido #1001',
+        imageUrl: 'assets/img/pedido1001.png', // 示例图片路径
+        description: 'Este es un producto de prueba con un diseño muy elegante.'
+      },
+      stars: 3,
+      opinion: 'Me gustó bastante, aunque podría ser más barato.'
+    },
+    {
+      article: {
+        name: 'Pedido #1002',
+        imageUrl: 'assets/img/pedido1002.png',
+        description: 'Un producto de alta calidad, con gran resistencia y durabilidad.'
+      },
+      stars: 5,
+      opinion: 'Excelente calidad, repetiré sin dudarlo.'
+    },
+    {
+      article: {
+        name: 'Pedido #1003',
+        imageUrl: 'assets/img/pedido1003.png',
+        description: 'Ideal para el día a día, combina funcionalidad y estilo.'
+      },
+      stars: 4,
+      opinion: 'Buena relación calidad-precio.'
+    }
   ];
 
+  // 点击星星时更新评分
   rateProduct(review: any, stars: number) {
     review.stars = stars;
   }
