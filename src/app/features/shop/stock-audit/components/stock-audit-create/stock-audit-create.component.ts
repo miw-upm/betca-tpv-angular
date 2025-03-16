8a7eb56b78fa87b7543e51b4aaabc493d53c22d6
ingjava <ing.jalvargas@gmail.com>
2025-03-16 23:00:53 +0100

    Add stock Audit Create miw-upm/betca-tpv#624

--- src/app/features/shop/stock-audit/components/stock-audit-create/stock-audit-create.component.ts
+++ src/app/features/shop/stock-audit/components/stock-audit-create/stock-audit-create.component.ts
@@ -2,16 +2,17 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialogActions, MatDialogClose, MatDialogRef, MatDialogTitle } from '@angular/material/dialog';
+import { MatDialogActions, MatDialogClose, MatDialogModule, MatDialogRef, MatDialogTitle } from '@angular/material/dialog';
 import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { StockAuditService } from '../../services/stock-audit.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   standalone: true,
   imports: [
-    MatDialogTitle,
+    MatDialogModule,
     MatFormFieldModule,
     FormsModule,
     MatInputModule,
@@ -28,7 +29,8 @@ export class StockAuditCreateComponent implements OnInit {
 
   constructor(
     private readonly dialogRef: MatDialogRef<StockAuditCreateComponent>,
-     private stockAuditService: StockAuditService
+    private stockAuditService: StockAuditService,
+    private readonly snackBar: MatSnackBar
    ) { }
 
   ngOnInit(): void {
@@ -37,6 +39,9 @@ export class StockAuditCreateComponent implements OnInit {
   onCreate(): void {
     this.stockAuditService.create()
     .subscribe(() =>{
+      this.snackBar.open("Stock audit create", "Success", {
+        duration: 5000
+      });
       this.dialogRef.close(true);
     })
   }
